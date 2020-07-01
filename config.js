6ef314779402094c13c2d47a0aa0e227dc6c2569
Enrico Mrass <emrass@users.noreply.github.com>
2020-07-01 09:34:31 -0700

    Remove Gatsby Google Analytics ID

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 5,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   useKatex: false,
   menu: [
     {
